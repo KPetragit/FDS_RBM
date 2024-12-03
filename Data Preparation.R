b42c58eb80f265bbbc60bc5e003017d29e95e8ea
KPetragit <kaps.petra1@gmail.com>
2024-12-03 17:00:43 +0100

    Data prep - adding derived & dissagregation variables. Changing from 2 to 5 datasets

--- Data Preparation.R
+++ Data Preparation.R
@@ -6,23 +6,39 @@ pacman::p_load(
   readr,labelled,pastecs,psych,tableone, outbreaks, ggplot2, unhcrthemes,
   scales, gt,webshot2, sjlabelled, waffle, writexl,remotes, haven )
 
-#Import "main" and "roster" data file. Here we are using FDS Pakistan dataset. 
+#Import 5 datasets. Here we are using FDS Pakistan dataset. 
 library(haven)
-#main <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Allrespondents_complete.dta")
+roster <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Roster_complete.dta")
+HoH <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_HoH_complete.dta"
+)
+RA_adult <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Random_Member_complete.dta"
+)
+RA_woman <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Random_Woman_complete.dta"
+)
+RA_caregiver <- read_dta ("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Random_Child_complete.dta"
+)
 
-#hhroster <- read_dta("C:/Users/KAPS/OneDrive - UNHCR/300 - ST - Survey Team - Main/Survey Programme Team/Projects/FDS/Countries/Pakistan/Data Management/4 Analysis/FDS_PAK_2024_Roster_complete.dta"
-#)
 
-# Rename _uuid to uuid in both datasets
-main <- main %>%
+# Rename _uuid to uuid in all datasets
+HoH <- HoH %>%
   rename(uuid = `_uuid`)
 
-hhroster <- hhroster %>%
+roster <- roster %>%
   rename(uuid = `_uuid`)
+
+RA_adult <- RA_adult %>%
+  rename(uuid = `_uuid`)
+
+RA_woman <- RA_woman %>%
+  rename(uuid = `_uuid`)
+
+RA_caregiver <- RA_caregiver %>%
+  rename(uuid = `_uuid`)
+
 
 ###Age of the child - Immunization 
 # Calculate the age of the child which was asked the questions on immunization
-main <- main %>%
+RA_caregiver <- RA_caregiver %>%
   mutate(
     childnametouseAGE = as.numeric(childnametouseAGE),  # Convert to numeric
     child_age_immunization = case_when(
@@ -39,9 +55,9 @@ main <- main %>%
 
 # Pull the age of the child about whom he child labour questions are asked 
 # Perform a left join and extract the child's age
-main <- main %>%
+HoH <- HoH %>%
   left_join(
-    hhroster %>% select(uuid, rosterposition, HH_04), # Select relevant columns from hhroster
+    roster %>% select(uuid, rosterposition, HH_04), # Select relevant columns from hhroster
     by = c("uuid" = "uuid", "selected_childla" = "rosterposition") # Match on uuid and positions
   ) %>%
   rename(child_labor_age = HH_04) # Rename HH_04 to child_labor_age
@@ -50,102 +66,145 @@ main <- main %>%
 ### Household members size
 # Create a new variable to count the number of household members in each household 
 ##Here we are using the variable "member" which assigned a 1 for each person that passes the check for actually being a household member. This check done automatically in the data collection form. 
-household_counts <- hhroster %>%
+household_counts <- roster %>%
   group_by(uuid) %>%
   summarise(HHmembersize = sum(member == 1, na.rm = TRUE))
 ##Join with the main data set
-main <- main %>%
+HoH <- HoH %>%
   left_join(household_counts, by = "uuid")
 
 
+#Disintegration variables 
+##1. Roster 
+### Population Groups - Population group variable is available in the HoH dataset. Here we pull it to the roster dataset. 
+roster <- roster %>%
+  left_join(
+    HoH %>% select(uuid, Intro_07), # Select relevant columns from HoH
+    by = c("uuid" = "uuid") # Match on uuid 
+  ) 
+roster <- roster %>%
+  rename(Intro_07_roster = Intro_07) #Rename to Intro_07_roster 
+
+##2. Age
+### Create two new variables for different age categories
+### 4 categories (0-4, 5-17, 18-59, 60+)
+
+roster$HH_04_cat4 <- cut(roster$HH_04, breaks = c(-1, 4, 17, 59, Inf), 
+                         labels = c("0-4", "5-17", "18-59", "60+"))
+
+### 2 categories (<18, >18)
+
+roster$HH_04_cat2 <- cut(roster$HH_04, breaks = c(-1, 17, Inf), 
+                         labels = c("0-17", "18-60+"))
+
+table(roster$HH_04_cat4)
+table(roster$HH_04_cat2)
+
+### 3. Disability 
+
 ### Washington Group Index
-#Disability status identifiers are calculated based on guidelines by the Washington Group on Disability Statistics for over 5-year-olds. Note that in FDS we have three levels of disability level (some difficulty, a lot of difficulty, cannot do at all).
+#Disability status identifiers are calculated based on guidelines by the [Washington Group on Disability Statistics](https://www.washingtongroup-disability.com/fileadmin/uploads/wg/WG_Document__6C_-_Analytic_Guidelines_for_the_WG-ES__Stata_.pdf) for over 5-year-olds. Note that in FDS we have five levels of disability level (some difficulty, a lot of difficulty, cannot do at all, don't know, refuse to answer).
+
+###Step 1: Add the level 0 - no difficulty, for those members above 5 years of age who were not identified as having disabilities. 
+#Vision
+roster <- roster %>% 
+  mutate(Dis_03 = if_else(is.na(Dis_03) & HH_04 > 5, 0, Dis_03))
+#Hearing
+roster <- roster %>% 
+  mutate(Dis_06 = if_else(is.na(Dis_06) & HH_04 > 5, 0, Dis_06))
+#Mobility
+roster <- roster %>% 
+  mutate(Dis_09 = if_else(is.na(Dis_09) & HH_04 > 5, 0, Dis_09))
+#Cognition
+roster <- roster %>% 
+  mutate(Dis_12 = if_else(is.na(Dis_12) & HH_04 > 5, 0, Dis_12))
+#Self-care
+roster <- roster %>% 
+  mutate(Dis_15 = if_else(is.na(Dis_15) & HH_04 > 5, 0, Dis_15))
+#Communication
+roster <- roster %>% 
+  mutate(Dis_18 = if_else(is.na(Dis_18) & HH_04 > 5, 0, Dis_18))
+
+###Step 2: Generate frequency distributions on each of the five WG-SS domain variables
+
+### 0 No difficulty 
+### 1	Some difficulty
+### 2	A lot of difficulties
+### 3	Cannot do at all
+### 98	Don’t know
+### 99	Refused to Answer
+
+#Vision 
+barplot(table(roster$Dis_03), main = "Vision")
+#Hearing
+barplot(table(roster$Dis_06), main = "Hearing")
+#Mobility
+barplot(table(roster$Dis_09), main = "Mobility")
+#Cognition
+barplot(table(roster$Dis_12), main = "Cognition")
+#Self-care
+barplot(table(roster$Dis_15), main = "Self-care")
+#Communication
+barplot(table(roster$Dis_18), main = "Communicating")
 
 
-#Conditions for different disability identifiers:
-  
-#Category 1: any domain = 1, 2 or 3
+##Step 3: Codes (99) Refuse to answer and (98) Don’t know, are recoded to Missing.
 
-#Category 2: at least two domains = 1 or at least one domain = 2 or 3
-
-#Category 3: at least one domain = 2 or 3
-
-#Category 4: at least one domain = 3
-
-#The WG recommends using category as the indicator for disability status.
-
-#Step 1: Sum disability cases across domains
-hhroster <- hhroster %>%
-  mutate(disabilitySeverity1_count = rowSums(
-    select(., all_of(c("Dis_03", "Dis_06", "Dis_09", "Dis_12", "Dis_15", "Dis_18"))) == 1, #looking at disability level 1 = Some difficulty
-    na.rm = TRUE))
-# "disabilitySeverity1_count" represents the number of domains with disability severity 1 (some difficulty).
-
-hhroster <- hhroster %>%
-  mutate(disabilitySeverity2_count = rowSums(
-    select(., all_of(c("Dis_03", "Dis_06", "Dis_09", "Dis_12", "Dis_15", "Dis_18"))) == 2, #looking at disability level 2 = A lot of difficulty
-    na.rm = TRUE))
-# "disabilitySeverity2_count" represents the number of domains with disability severity 2 (A lot of difficulty).
-
-hhroster <- hhroster %>%
-  mutate(disabilitySeverity3_count = rowSums(
-    select(., all_of(c("Dis_03", "Dis_06", "Dis_09", "Dis_12", "Dis_15", "Dis_18"))) == 3, #looking at disability level 3 = Cannot do at all
-    na.rm = TRUE))
-# "disabilitySeverity3_count" represents the number of domains with disability severity 3 (Cannot do at all).
-
-
-#Step 2: Classify disability identifiers 
-#WG Disability Status Identifier 1 = Category 1
-
-hhroster <- hhroster %>%
+roster <- roster %>%
   mutate(
-    disability1 = case_when(
-      HH_04 >= 5 & (disabilitySeverity1_count > 0 | 
-                         disabilitySeverity2_count > 0 | 
-                         disabilitySeverity3_count > 0) ~ 1, # Disability exists
-      HH_04 >= 5 & (disabilitySeverity1_count == 0 & 
-                         disabilitySeverity2_count == 0 & 
-                         disabilitySeverity3_count == 0) ~ 2, # No disability
-      TRUE ~ NA_real_ # Default for missing or unmatched cases
-    )
+    Dis_03 = ifelse(Dis_03 == 98 | Dis_03 == 99, NA, Dis_03),
+    Dis_06 = ifelse(Dis_06  == 98 | Dis_06  == 99, NA, Dis_06),
+    Dis_09  = ifelse(Dis_09 == 98 | Dis_09 == 99, NA, Dis_09),
+    Dis_12 = ifelse(Dis_12 == 98 | Dis_12 == 99, NA, Dis_12),
+    Dis_15 = ifelse(Dis_15 == 98 | Dis_15 == 99, NA, Dis_15),
+    Dis_18 = ifelse(Dis_18 == 98 | Dis_18 == 99, NA, Dis_18)
   )
-var_label(hhroster$disability1) <- "WGI Disability Status Identifier 1"
 
-#WG Disability Status Identifier 2 = Category 2
-hhroster <- hhroster %>%
-  mutate(
-    disability2 = case_when(
-      HH_04 >= 5 & (disabilitySeverity1_count > 1 | 
-                         disabilitySeverity2_count > 0 | 
-                         disabilitySeverity3_count > 0) ~ 1, # Disability exists
-      HH_04 >= 5 & (disabilitySeverity1_count <= 1 & 
-                         disabilitySeverity2_count == 0 & 
-                         disabilitySeverity3_count == 0) ~ 2, # No disability
-      TRUE ~ NA_real_ # Default case for missing or unmatched conditions
-    )
-  )
-var_label(hhroster$disability2) <- "WGI Disability Status Identifier 2"
+## Step 4: Create disability status indicator for the Washington Group short set on disability
 
-#WG Disability Status Identifier 3 = Category 3
-hhroster <- hhroster %>%
-  mutate(
-    disability3 = case_when(
-      HH_04 >= 5 & (disabilitySeverity2_count > 0 | 
-                         disabilitySeverity3_count > 0) ~ 1, # Disability exists
-      HH_04 >= 5 & (disabilitySeverity2_count == 0 & 
-                         disabilitySeverity3_count == 0) ~ 2, # No disability
-      TRUE ~ NA_real_ # Default case for unmatched conditions
-    )
-  )
-var_label(hhroster$disability3) <- "WGI Disability Status Identifier 3"
+roster <- roster %>%
+  mutate(disability = case_when(
+    Dis_03 %in% c(3, 4) |
+      Dis_06 %in% c(3, 4) |
+      Dis_09 %in% c(3, 4) |
+      Dis_12 %in% c(3, 4) |
+      Dis_15 %in% c(3, 4) |
+      Dis_18 %in% c(3, 4) ~ 1,
+    Dis_03 %in% c(0, 1, 2) |
+      Dis_06 %in% c(0, 1, 2)|
+      Dis_09 %in% c(0, 1, 2)|
+      Dis_12 %in% c(0, 1, 2)|
+      Dis_15 %in% c(0, 1, 2)|
+      Dis_18 %in% c(0, 1, 2) ~ 2, 
+    TRUE ~ NA_real_
+  ))
 
-#WG Disability Status Identifier 4 = Category 4
-hhroster <- hhroster %>%
-  mutate(
-    disability4 = case_when(
-      HH_04 >= 5 & (disabilitySeverity3_count > 0) ~ 1,  # Disability exists
-      HH_04 >= 5 & (disabilitySeverity3_count == 0) ~ 2, # No disability
-      TRUE ~ NA_real_ # Default case for unmatched conditions
+
+### 4. Country of Origin 
+#The individual information on the country of origin comes from the roster. To have one single variable for country of origin information, the country code for the country of enumeration (i.e. PAK for Pakistan) will be entered as below. This question is asked only to individuals older than 15. For individuals younger than 15, the value is equaled to the country of origin of the household (as responded by the Head of the Household)
+
+roster <- roster %>%
+  left_join(
+    HoH %>% select(uuid, origincntry), # Select relevant columns from HoH
+    by = c("uuid" = "uuid") # Match on uuid 
+  ) 
+roster <- roster %>%
+  rename(origincountry_roster = origincntry) #Rename to Intro_07_roster
+
+roster <- roster %>%
+  mutate( # country of origin from ID_00 and ID_00_specify
+    COO = case_when(
+      ID_00 == 1 ~ "PAK", ##ensure to adjust here the country code (where FDS took place)
+      ID_00 == 2 ~ as.character(roster$ID_00_specify),
+      ID_00 == 3 ~ "Stateless",
+      ID_00 == 99 ~ "99",
+      ID_00 == 98 ~ "98", 
+      is.na(ID_00) ~ as.character(roster$origincountry_roster), #For individuals under 15 years, ID_00 is not asked and therefore NA. For these individuals, we take "origincntry" which is a the country of origin of the household (answered by the HoH)
     )
-  )
-var_label(hhroster$disability4) <- "WGI Disability Status Identifier 4"
+  ) 
+
+
+
+
+
+
